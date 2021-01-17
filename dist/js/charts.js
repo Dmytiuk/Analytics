91579f22187ed54e38dec1f9401099fe3fe54701
Valentyn <valaimg2218@gmail.com>
2021-01-17 17:04:36 +0200

    desktop

--- dist/js/charts.js
+++ dist/js/charts.js
@@ -461,3 +461,142 @@ var radarChart = new Chart(marksCanvas, {
   },
 }
 });
+var marksCanvas = document.getElementById("bigChart1");
+
+var marksData = {
+  labels: ["65", "75", "70", "80", "60", "80"],
+  datasets: [{
+    label: "Data1",
+    backgroundColor: "rgba(0, 136, 240, 1)",
+    data: [65, 75, 70, 80, 60, 80],
+    fill: false,
+    borderColor: "rgba(255, 255, 255, 1)",
+    borderWidth: "3"
+  }, {
+    label: "Data2",
+    backgroundColor: "rgba(239, 42, 130, 1)",
+    data: [54, 70, 70, 75],
+    fill: false,
+    borderColor: "rgba(255, 255, 255, 1)",
+    borderWidth: "3"
+  }, {
+    label: "Data3",
+    backgroundColor: "rgba(23, 39, 85, 1)",
+    data: [10, 15, 110, 75],
+    fill: false,
+    borderColor: "rgba(255, 255, 255, 1)",
+    borderWidth: "3"
+  }
+    ]
+};
+
+var radarChart = new Chart(marksCanvas, {
+  type: 'doughnut',
+  data: marksData,
+  options: {
+  legend: {
+    display: false
+  },
+}
+});
+var ctx = document.getElementById('bigChart2').getContext('2d');
+var mybigChart2 = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+        labels: ['54-55', '45-54', '35-44', '21-87', '32-23', '90-21'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 8, 2, 7],
+            backgroundColor: [
+                'rgba(0, 136, 240, 1)',
+                'rgba(239, 42, 130, 1)',
+                'rgba(0, 136, 240, 1)',
+                'rgba(239, 42, 130, 1)',
+                'rgba(0, 136, 240, 1))',
+                'rgba(239, 42, 130, 1)'
+            ],
+            borderColor: [
+                'rgba(0, 136, 240, 1)',
+                'rgba(239, 42, 130, 1)',
+                'rgba(0, 136, 240, 1)',
+                'rgba(239, 42, 130, 1))',
+                'rgba(0, 136, 240, 1)',
+                'rgba(239, 42, 130, 1))'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+      legend: {
+        display: false
+      },
+      scales: {
+          yAxes: [{
+            display: false,
+              ticks: {
+                  beginAtZero: true
+              },
+              gridLines: {
+              color: "rgba(0, 0, 0, 0)",
+          }
+        }],
+        xAxes: [{
+            display: false,
+            gridLines: {
+            color: "rgba(0, 0, 0, 0)",
+        }
+        }]
+      }
+    }
+});
+var ctx = document.getElementById('bigChart3').getContext('2d');
+var mybigChart3 = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['54-55', '45-54', '35-44', '21-87', '32-23', '90-21'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 8, 2, 7],
+                fill: false,
+            backgroundColor: [
+                'rgba(0, 136, 240, 1)',
+                'rgba(239, 42, 130, 1)',
+                'rgba(0, 136, 240, 1)',
+                'rgba(239, 42, 130, 1)',
+                'rgba(0, 136, 240, 1))',
+                'rgba(239, 42, 130, 1)'
+            ],
+            borderColor: [
+                'rgba(0, 136, 240, 1)',
+                'rgba(239, 42, 130, 1)',
+                'rgba(0, 136, 240, 1)',
+                'rgba(239, 42, 130, 1))',
+                'rgba(0, 136, 240, 1)',
+                'rgba(239, 42, 130, 1))'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+      legend: {
+        display: false
+      },
+        scales: {
+            yAxes: [{
+              display: false,
+                ticks: {
+                    beginAtZero: true
+                },
+                gridLines: {
+                color: "rgba(0, 0, 0, 0)",
+            }
+          }],
+          xAxes: [{
+            display: false,
+              gridLines: {
+              color: "rgba(0, 0, 0, 0)",
+          }
+          }]
+        }
+    }
+});
